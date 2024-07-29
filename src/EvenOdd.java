c7262eccc30ce43bddc2e72fbc97ecd2e8de17f3
arlawnsgka <arlawnsgkaa@naver.com>
2024-07-29 11:17:19 +0900

    11commit

---
+++ src/EvenOdd.java
@@ -0,0 +1,12 @@
+public class EvenOdd {
+    public String solution(int num) {
+        if (num % 2 == 0) {
+            return ("Even");
+        } else {
+            return ("Odd");
+        }
+    }
+
+
+
+}
