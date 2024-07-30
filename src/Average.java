cb007a6b8d2ce34cdd6f8e4a5b5d46981bf09f32
arlawnsgka <arlawnsgkaa@naver.com>
2024-07-30 09:09:43 +0900

    12commit

--- src/Average.java
+++ src/Average.java
@@ -1,6 +1,11 @@
 public class Average {
-        public double solution(int[] numbers) {
-
+    public double solution(int[] arr) {
+        double answer = 0;
+        for (int i = 0; i < arr.length; i++) {
+            answer += arr[i];
         }
+        answer = answer / arr.length;
+        return answer;
+    }
 
 }
